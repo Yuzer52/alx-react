22e1b9996ca8db8028c4737db00900ff287af6bc
Yusuf <yuzer5252@gmail.com>
2024-08-29 14:05:02 +0300

    this is for 0x02

---
+++ 0x02-react_props/task_3/dashboard/src/Notifications.test.js
@@ -0,0 +1,31 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+import Notifications from './Notifications';
+
+describe('Notification tests', () => {
+	it('renders Notification component without crashing', () => {
+		const component = shallow(<Notifications />);
+
+		expect(component).toBeDefined();
+	});
+
+	it('renders ul', () => {
+		const component = shallow(<Notifications />);
+
+		expect(component.find('ul')).toBeDefined();
+	});
+
+	it('renders three list items', () => {
+		const component = shallow(<Notifications />);
+
+		expect(component.find('li')).toHaveLength(3);
+	});
+
+	it('renders correct text', () => {
+		const component = shallow(<Notifications />);
+
+		expect(component.find('p').prop('children')).toBe(
+			'Here is the list of notifications'
+		);
+	});
+});
