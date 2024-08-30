bfe79e79b0757359b51abed8de16617290aa13df
Yusuf <yuzer5252@gmail.com>
2024-08-30 23:31:24 +0300

    removed

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++
@@ -1,9 +0,0 @@
-import { shallow } from 'enzyme';
-import React from 'react';
-import App from './App';
-
-const wrapper = shallow(<App />);
-
-it('renders without crashing', () => {
-  shallow(<App />);
-});
